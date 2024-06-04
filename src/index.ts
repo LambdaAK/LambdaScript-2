ea6fd0f3851c5a0355adaafbd87398edc5b4a637
Alex Kozik <ajk333@cornell.edu>
2024-06-04 16:31:53 -0400

    Worked on generate.

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,13 @@ import { L9Expr } from "./AST/expr/L9"
 import { exprParser } from "./parser/expr/L9"
 import { typeL4Parser } from "./parser/type/TypeL4"
 import { typeL1Parser } from "./parser/type/TypeL1"
+import { generate } from "./typecheck/typecheck"
 
-const s: string = "a -> b -> c -> d"
+const s: string = ""
 
 const tokens = lex(s)
 
-const nodeMaybe = typeL4Parser(tokens)
+const nodeMaybe = exprParser(tokens)
 
 if (nodeMaybe.type === 'None') {
   console.log('parse error')
@@ -20,6 +21,9 @@ else {
   const node = nodeMaybe.value[0]
   console.dir(node, { depth: null })
   //console.log(stringOfNode(node))
+  const [t, equations] = generate(node, new Map())
+  console.log(t)
+  console.log(equations)
 }
 
 export const lexAndParse = (s: string): Maybe<L9Expr> => {
--- src/typecheck/typecheck.ts
+++ src/typecheck/typecheck.ts
@@ -1,12 +1,48 @@
 import { L9Expr } from "../AST/expr/L9"
+import { PatL2 } from "../AST/pat/PatL2"
 
-type Type = TypeL2
+type Type = TypeL4
 type TypeEquation = [Type, Type]
 
 type Expr = L9Expr
 
+type Pat = PatL2
+
 // a mapping from identifiers to types
 type StaticEnv = Map<string, Type>
+
+let typeVarCounter = 1
+const freshTypeVar: () => Type = () => {
+  const typeVar: Type = {
+    type: 'TypeVar',
+    name: String(typeVarCounter)
+  }
+  typeVarCounter++
+  return typeVar
+}
+
+const generatePattern = (pattern: Pat): [Type, TypeEquation[]] => {
+  switch (pattern.type) {
+    case "BoolPat":
+      return [{ type: 'BoolType' }, []]
+    case "IntPat":
+      return [{ type: 'IntType' }, []]
+    case "StringPat":
+      return [{ type: 'StringType' }, []]
+    case "UnitPat":
+      return [{ type: 'UnitType' }, []]
+    case "IdPat":
+      return [freshTypeVar(), []]
+    case "WildcardPat":
+      return [freshTypeVar(), []]
+    case "ParenPat":
+      return generatePattern(pattern.node)
+    case "ConsPat":
+      throw new Error('ConsPat not implemented in generatePattern')
+    case "NilPat":
+      throw new Error('NilPat not implemented in generatePattern')
+  }
+}
 
 /**
  * Generate type equations for an expression
@@ -29,8 +65,46 @@ export const generate = (expr: Expr, staticEnv: StaticEnv): [Type, TypeEquation[
         throw new Error(`Type equation generation failed: identifier ${expr.value} not found in static environment`)
       }
       return [type, []]
-    default:
-      throw new Error("Type equation generation unimplemented")
+    case 'PlusNode':
+      const [leftType, leftEquations] = generate(expr.left, staticEnv)
+      const [rightType, rightEquations] = generate(expr.right, staticEnv)
 
+
+      return [
+        { type: 'IntType' },
+        [
+          [leftType, { type: 'IntType' }],
+          [rightType, { type: 'IntType' }],
+          ...leftEquations,
+          ...rightEquations
+        ]
+      ]
+
+
+    case 'FunctionNode':
+      const [outputType, outputEquations] = generate(expr.body, staticEnv)
+      const [inputType, inputEquations] = generatePattern(expr.pattern)
+
+      /*
+        The type of the function is inputType -> outputType
+      */
+
+      const t: Type = {
+        type: 'FunctionType',
+        left: {
+          type: "ParenType",
+          t: inputType
+        },
+        right: {
+          type: "ParenType",
+          t: outputType
+        }
+      }
+
+      return [t, [...inputEquations, ...outputEquations]]
+
+      throw new Error('FunctionNode not implemented in generate')
+    default:
+      throw new Error('Unimplemented in generate')
   }
 }
